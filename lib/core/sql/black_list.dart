3a0ce2ff7b8267b2aeff5e087463ff8b39a60396
Yui <44658840+HereIsYui@users.noreply.github.com>
2024-12-22 18:29:24 +0800

    feat(core): 优化黑名单功能- 添加 getAllUser 方法获取所有用户信息 - 添加 addUser 方法将用户添加到黑名单 - 修改 blackList 属性以使用新的 getAllUser 方法

--- lib/core/sql/black_list.dart
+++ lib/core/sql/black_list.dart
@@ -1,3 +1,4 @@
+import 'package:fishpi/fishpi.dart';
 import 'package:hive/hive.dart';
 
 class BlackList {
@@ -7,7 +8,20 @@ class BlackList {
     msgBox = await Hive.openBox('blackList');
   }
 
-  static get blackList => msgBox.values.toList().reversed.toList();
+  static get blackList => getAllUser();
+
+  static getAllUser() {
+    List<UserInfo> list = [];
+    var blackList = msgBox.values.toList();
+    for (var item in blackList) {
+      list.add(UserInfo.from(item));
+    }
+    return list;
+  }
+
+  static addUser(UserInfo user) async {
+    return await msgBox.put(user.oId, user);
+  }
 
   static removeUser(int index) async {
     return await msgBox.delete(index);
